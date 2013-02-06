715870a346fbc69fb0a661270f633426f43b0a66
Thom Nichols <tmnichols@gmail.com>
2013-02-06 16:50:19 -0500

    bound random vals

--- web/test/random_data.py
+++ web/test/random_data.py
@@ -8,9 +8,9 @@ last_vals = [23.4, 0.8, 0]
 def random_val():
     global last_vals
     last_vals = [
-        last_vals[0] + random.gauss(0,2),
-        last_vals[1] + random.gauss(0,.03),
-        int(round(min(0,max(1,random.gauss(last_vals[2],.3)))))]
+        max(20, min(50, last_vals[0] + random.gauss(0,2))),
+        max(0,  min(1,  last_vals[1] + random.gauss(0,.03))),
+        int(round(max(0,min(1,random.gauss(last_vals[2],.5)))))]
         
     fmt = '%0.2f %0.2f %d' % tuple(last_vals)
     print fmt
